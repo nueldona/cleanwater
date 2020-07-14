4769464c66233add17f61e9784db2ec2a6d0efec
Babatunde Omonijo <SONY@IT-BABATUNDE-OMONIJO.CHQ.NNPCGroup.local>
2020-07-14 22:56:51 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// water bubbles
+
+function createBubble() {
+  const header = document.querySelector('header');
+  const createElement = document.createElement('span');
+
+  let size = Math.random() * 60;
+
+  createElement.style.width = size + 'px';
+  createElement.style.height = size + 'px';
+  createElement.style.left = Math.random() * innerWidth + 'px';
+
+  header.appendChild(createElement);
+
+  setTimeout(() => {
+    createElement.remove()
+  }, 4000);
+}
+
+setInterval(createBubble, 20)
