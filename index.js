044fd5f1a1c6ab21ba39e4cf90e98091c2b9f88c
Babatunde Omonijo <SONY@IT-BABATUNDE-OMONIJO.CHQ.NNPCGroup.local>
2020-07-16 14:08:17 +0100

    fourth commit

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-// water bubbles
+// // water bubbles
 
-function createBubble() {
-  const header = document.querySelector('header');
-  const createElement = document.createElement('span');
+// function createBubble() {
+//   const header = document.querySelector('header');
+//   const main = document.querySelector('.main');
 
-  let size = Math.random() * 80;
+//   const createElement = document.createElement('span');
+//   const createBigElement = document.createElement('span');
 
-  createElement.style.width = size + 'px';
-  createElement.style.height = size + 'px';
-  createElement.style.left = Math.random() * innerWidth + 'px';
+//   let size = Math.random() * 80;
 
-  header.appendChild(createElement);
+//   // size
+//   createElement.style.width = size + 'px';
+//   createElement.style.height = size + 'px';
+//   createElement.style.left = Math.random() * innerWidth + 'px';
 
-  setTimeout(() => {
-    createElement.remove()
-  }, 4000);
-}
 
-setInterval(createBubble, 20)
+//   // appending elements
+//   header.appendChild(createElement);
+
+//   setTimeout(() => {
+//     createElement.remove()
+//   }, 3000);
+// }
+
+// setInterval(createBubble, 20)
+
+
+// // big size bubble
+
+// function createBigBubble() {
+//   const main = document.querySelector('.main');
+//   const createBigElement = document.createElement('span');
+
+//   let bigSize = Math.random() * 100;
+
+//   // big size
+//   createBigElement.style.width = bigSize + 'px';
+//   createBigElement.style.height = bigSize + 'px';
+//   createBigElement.style.left = Math.random() * innerWidth + 'px';
+
+//   // appending elements
+//   main.appendChild(createBigElement);
+
+//   setTimeout(() => {
+//     createBigElement.remove()
+//   }, 8000);
+// }
+
+// setInterval(createBigBubble, 20)
